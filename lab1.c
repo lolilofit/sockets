b2069fd4327493344ca8d0d45f55bc87e2d44efd
Darya <usovadasha@myttk.ru>
2020-02-03 23:19:06 +0700

    clean code

--- lab1.c
+++ lab1.c
@@ -17,8 +17,6 @@ struct adresses {
 
 struct adresses* add_adr(struct adresses* list, char* buf) {
 	struct adresses* new_adr = (struct adresses*)malloc(sizeof(struct adresses));
-//	new_adr->adr = buf;
-	//sprintf(new_adr->adr, "%s", buf);
 	int i;
 	for(i = 0; i < strlen(buf); i++) {
 		(new_adr->adr)[i]=buf[i];
@@ -28,7 +26,6 @@ struct adresses* add_adr(struct adresses* list, char* buf) {
 	new_adr->next = NULL;
 	if(list == NULL) {
 		list = new_adr;
-	//	printf("added - %s\n", list->adr);
 		return list;
 	}
 	struct adresses* cur = list;
@@ -39,19 +36,14 @@ struct adresses* add_adr(struct adresses* list, char* buf) {
 }
 
 int find_adr(struct adresses* list, char *adr) {
-//	printf("cmp\n");
 	struct adresses* cur = list;
 	if(cur == NULL) {
-//		printf("null list\n");
 		return 1;
 	}
 	while(cur) {
-//		printf("compare %s %s\n", adr, cur->adr);
 		if(strcmp(adr, cur->adr) == 0) {
-//			printf("deq strings!\n");
 			return 0;
 		}
-//		printf("diff str!\n");
 		cur = cur->next;
 	}
 	return 1;
@@ -87,21 +79,14 @@ int main(int argc, char* argv[]) {
 	}
 	int broadcastEnable=1;
 	int ret=setsockopt(sc, SOL_SOCKET, SO_BROADCAST, &broadcastEnable, sizeof(broadcastEnable));
-//	struct timeval read_timeout;
-//	read_timeout.tv_sec = 0;
-//	read_timeout.tv_usec = 10;
-//	setsockopt(sc, SOL_SOCKET, SO_RCVTIMEO, &read_timeout, sizeof(read_timeout));
-
 
 	memset(&adr, '0', sizeof(adr));
 	adr.sin_family = AF_INET;
-//	adr.sin_addr.s_addr = inet_addr("192.168.1.16");
 	adr.sin_addr.s_addr = INADDR_ANY;
 	adr.sin_port = htons(1900);
 
 	memset(&other_adr, '0', sizeof(other_adr));
 	other_adr.sin_family = AF_INET;
-	//other_adr.sin_addr.s_addr = inet_addr("255.255.255.255");
 	other_adr.sin_addr.s_addr = inet_addr("192.168.1.255");
 	other_adr.sin_port = htons(1900);
 	memset(&recv, '0', sizeof(recv));
@@ -114,19 +99,13 @@ int main(int argc, char* argv[]) {
 		res = sendto(sc, mes, strlen(mes)+1, 0, (struct sockaddr*)&other_adr, sizeof(other_adr));
 		if(res < 0)
 			printf("erroe sending\n");
-		//printf("send\n");
-
+	
 		count = 0;
 		time_now = time(NULL);
 		tmp = NULL;
 		while((time(NULL) - time_now) < TIMEOUT) {
 			socklen = sizeof(recv);
 			if(recvfrom(sc, buf, strlen(buf)+1, MSG_DONTWAIT, (struct sockaddr*)&recv, &socklen) >= 0) {
-			//	printf("err\n");
-		//		return 2;
-		//	}
-		//	printf("rescv\n");
-		//	printf("addr is: %s\n", inet_ntoa(recv.sin_addr));
 			if(find_adr(tmp, inet_ntoa(recv.sin_addr)))
 				tmp = add_adr(tmp, inet_ntoa(recv.sin_addr));
 
@@ -144,30 +123,17 @@ int main(int argc, char* argv[]) {
 			}
 
 		}
-//printf("---->>>>>>>\n");
-//		close(sc);
-//		return 0;
-//		printf("find diff\n");
-//		printf("%%%%%%%%\n");
+
 		struct adresses* c;
-		//while(c) {
-		//printf("%s\n", c->adr);
-		//c=c->next;
-		//}
-		//printf("^^^^^^^^^^\n");
 		c=list_adr;
 		while(c) {
 			count++;
-			//printf("%s\n", c->adr);
 			c=c->next;
 		}
-		//printf("%%%%%%%%\n");
-
+		
 		struct adresses* cur = list_adr;
 		for(i = 0; i < count; i++) {
-		//	printf("compare tmp and %s\n", cur->adr);
 			if(find_adr(tmp, cur->adr) == 1) {
-			//	printf("fout difference\n");
 				i = count + 1;
 				list_adr = tmp;
 				struct adresses* tmp_cur = list_adr;
